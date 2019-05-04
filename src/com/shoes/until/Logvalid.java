322ff9d9e527d4009fef3408065c4b66c6d7bc37
bjfrbjx <13634160841@163.com>
2019-05-04 21:10:06 +0800

    修改循环redirect
    Signed-off-by: bjfrbjx <13634160841@163.com>

---
+++ src/com/shoes/until/Logvalid.java
@@ -0,0 +1,56 @@
+package com.shoes.until;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet Filter implementation class Logvalid
+ */
+@WebFilter({"/main/*","/admin/*"})
+public class Logvalid implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public Logvalid() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req=(HttpServletRequest)request;
+		HttpServletResponse rep=(HttpServletResponse)response;
+		System.out.println("filter user");
+		if(req.getSession().getAttribute("user")==null) {
+			rep.sendRedirect("../index.jsp?encoded=true");
+		}
+		else {
+			chain.doFilter(request, response);
+		}
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
